4a71e5eae3c5ee4120f8c03775ab94c917b9ab97
sovmulax <soumailaevane@gmail.com>
2023-08-15 22:35:19 +0000

    feat: celery config

--- app/core/celery.py
+++ app/core/celery.py
@@ -1,7 +1,6 @@
+from __future__ import absolute_import, unicode_literals
 import os
-
 from celery import Celery
-from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
@@ -10,4 +9,3 @@ app = Celery("core")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
-
