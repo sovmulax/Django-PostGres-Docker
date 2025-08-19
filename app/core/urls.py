e7e766ae68adec938415a001ef30f2a31a80d440
sovmulax-pro <soumailaevane@gmail.com>
2025-08-19 11:41:50 +0000

    fix: corriger les espaces dans les chemins d'entrée et mettre à jour le test de vérification de l'état dans docker-compose

--- app/core/urls.py
+++ app/core/urls.py
@@ -8,5 +8,6 @@ admin.site.site_url = "/appname/"
 
 urlpatterns = [
     path("appname/admin/", admin.site.urls),
+    path("appname/session_security/", include("session_security.urls")),
     path("appname/", include("appname.urls")),
 ] + static(settings.MEDIA_URL, view=serve, document_root=settings.MEDIA_ROOT)
